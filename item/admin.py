8af99c5257913226df5bc17591504a819aea1e24
karl ducta <karlducta022@gmail.com>
2023-11-20 20:47:39 +0800

    added items in admin

--- item/admin.py
+++ item/admin.py
@@ -7,3 +7,20 @@ from . import models
 @admin.register(models.Category)
 class CatergoryAdmin(admin.ModelAdmin):
     list_display = ["name"]
+    search_fields = ["name__istartswith"]
+
+
+@admin.register(models.Items)
+class ItemsAdmin(admin.ModelAdmin):
+    autocomplete_fields = ["category", "created_by"]
+    list_display = [
+        "category",
+        "name",
+        "description",
+        "price",
+        "image",
+        "is_sold",
+        "created_by",
+        "created_at",
+    ]
+    list_select_related = ["category", "created_by"]
--- item/models.py
+++ item/models.py
@@ -27,3 +27,6 @@ class Items(models.Model):
 
     def __str__(self) -> str:
         return self.name
+
+    class Meta:
+        verbose_name_plural = "Items"
