30b1d2c05809f5978110d900589e8858f3ec0cb5
karl ducta <karlducta022@gmail.com>
2023-11-21 22:01:27 +0800

    added categories and items in front page

--- core/views.py
+++ core/views.py
@@ -1,8 +1,14 @@
 from django.shortcuts import render
 
+from item.models import Category, Items
+
 
 def index(request):
-    return render(request, "core/index.html")
+    items = Items.objects.filter(is_sold="False")[0:6]
+    categories = Category.objects.all()
+    return render(
+        request, "core/index.html", context={"categories": categories, "items": items}
+    )
 
 
 def contact(request):
--- marketplace/urls.py
+++ marketplace/urls.py
@@ -14,7 +14,9 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from django.conf import settings
 from django.contrib import admin
+from django.conf.urls.static import static
 from django.urls import path, include
 
 from core.views import index, contact
@@ -23,4 +25,4 @@ urlpatterns = [
     path("", index, name="index"),
     path("contact/", contact, name="contact"),
     path("admin/", admin.site.urls),
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
