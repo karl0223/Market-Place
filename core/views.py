a8df19dba818e941a1b1caf67695bfd938db15e3
karl ducta <karlducta022@gmail.com>
2023-11-27 00:11:36 +0800

    added signup

--- core/views.py
+++ core/views.py
@@ -1,6 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import redirect, render
 
 from item.models import Category, Item
+from .forms import SignupForm
 
 
 def index(request):
@@ -15,9 +16,19 @@ def contact(request):
     return render(request, "core/contact.html")
 
 
+def signup(request):
+    if request.method == "POST":
+        form = SignupForm(request.POST)
+
+        if form.is_valid():
+            form.save()
+
+            return redirect("/login/")
+    else:
+        form = SignupForm()
+
+    return render(request, "core/signup.html", {"form": form})
+
+
 def login(request):
     return render(request, "core/login.html")
-
-
-def signup(request):
-    return render(request, "core/signup.html")
