f9bb60e5d4a0e2e5067ff4db299f754fd85a77d2
karl ducta <karlducta022@gmail.com>
2023-11-28 22:42:19 +0800

    added login

--- core/urls.py
+++ core/urls.py
@@ -1,5 +1,7 @@
 from django.urls import path
+from django.contrib.auth import views as auth_views
 from . import views
+from .forms import LoginForm
 
 app_name = "core"
 
@@ -7,4 +9,11 @@ urlpatterns = [
     path("", views.index, name="index"),
     path("contact/", views.contact, name="contact"),
     path("signup/", views.signup, name="signup"),
+    path(
+        "login/",
+        auth_views.LoginView.as_view(
+            template_name="core/login.html", authentication_form=LoginForm
+        ),
+        name="login",
+    ),
 ]
--- marketplace/settings.py
+++ marketplace/settings.py
@@ -29,6 +29,10 @@ DEBUG = True
 ALLOWED_HOSTS = []
 
 
+LOGIN_URL = "/login/"
+LOGIN_REDIRECT_URL = "/"
+LOGOUT_REDIRECT_URL = "/"
+
 # Application definition
 
 INSTALLED_APPS = [
