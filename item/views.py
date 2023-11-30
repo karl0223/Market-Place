4c628870af35f6fa581c70f238afbdc5b0fbf64e
karl ducta <karlducta022@gmail.com>
2023-12-01 00:21:25 +0800

    fixed adding new items

--- item/views.py
+++ item/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import redirect, render, get_object_or_404
 from django.contrib.auth.decorators import login_required
 
 from .forms import NewItemForm
@@ -20,6 +20,16 @@ def detail(request, pk):
 
 @login_required
 def new(request):
-    form = NewItemForm
+    if request.method == "POST":
+        form = NewItemForm(request.POST, request.FILES)
+
+        if form.is_valid():
+            item = form.save(commit=False)
+            item.created_by = request.user
+            item.save()
+
+            return redirect("item:detail", pk=item.id)
+    else:
+        form = NewItemForm()
 
     return render(request, "item/form.html", {"form": form, "title": "New item"})
