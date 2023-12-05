56b8f320701941acbc3a9407c4eae1bba56f624b
karl ducta <karlducta022@gmail.com>
2023-12-05 22:30:19 +0800

    added search

--- item/views.py
+++ item/views.py
@@ -1,10 +1,34 @@
 from django.shortcuts import redirect, render, get_object_or_404
 from django.contrib.auth.decorators import login_required
-
+from django.db.models import Q
 from .forms import EditItemForm, NewItemForm
-from .models import Item
+from .models import Category, Item
 
 # Create your views here.
+
+
+def items(request):
+    query = request.GET.get("query", "")
+    category_id = request.GET.get("category", 0)
+    categories = Category.objects.all()
+    items = Item.objects.filter(is_sold=False)
+
+    if category_id:
+        items = items.filter(category_id=category_id)
+
+    if query:
+        items = items.filter(Q(name__icontains=query) | Q(description__icontains=query))
+
+    return render(
+        request,
+        "item/items.html",
+        {
+            "items": items,
+            "query": query,
+            "categories": categories,
+            "category_id": int(category_id),
+        },
+    )
 
 
 def detail(request, pk):
