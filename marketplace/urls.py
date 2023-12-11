7a1c896ea31a716b6da3d4d498ee347fc4c609b1
karl ducta <karlducta022@gmail.com>
2023-12-12 00:23:13 +0800

    BUG: Media files not loading --- Fix 1

--- marketplace/urls.py
+++ marketplace/urls.py
@@ -19,13 +19,13 @@ from django.contrib import admin
 from django.conf.urls.static import static
 from django.urls import path, include
 
-from core.views import login, signup
-from marketplace.settings import common
-
 urlpatterns = [
     path("", include("core.urls")),
     path("items/", include("item.urls")),
     path("inbox/", include("conversation.urls")),
     path("dashboard/", include("dashboard.urls")),
     path("admin/", admin.site.urls),
-] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
