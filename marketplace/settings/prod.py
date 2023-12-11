879a19fe57a7e0e2bc8c1da9d3d341d24a6e33bd
karl ducta <karlducta022@gmail.com>
2023-12-11 17:03:39 +0800

    test deploy

--- marketplace/settings/prod.py
+++ marketplace/settings/prod.py
@@ -1,8 +1,11 @@
 from .common import *
 import os
+import dj_database_url
 
 DEBUG = False
 
 SECRET_KEY = os.environ["SECRET_KEY"]
 
 ALLOWED_HOSTS = ["market-place-8hq1.onrender.com"]
+
+DATABASES = {"default": dj_database_url.config()}
