2eab576b483e66c7d1ad467b7820ef7522640cd1
karl ducta <karlducta022@gmail.com>
2023-12-12 01:30:37 +0800

    BUG: Media files not loading -- Fix 2

--- item/models.py
+++ item/models.py
@@ -20,7 +20,7 @@ class Item(models.Model):
     name = models.CharField(max_length=255)
     description = models.TextField(blank=True, null=True)
     price = models.DecimalField(max_digits=10, decimal_places=2)
-    image = models.ImageField(upload_to="item_images", blank=True, null=True)
+    image = models.ImageField(upload_to="item/images", blank=True, null=True)
     is_sold = models.BooleanField(default=False)
     created_by = models.ForeignKey(User, on_delete=models.CASCADE)
     created_at = models.DateTimeField(auto_now_add=True)
