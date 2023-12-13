c2c4e2045e259fd034b65b5f4aa620aa80fafdea
karl ducta <karlducta022@gmail.com>
2023-12-13 15:01:22 +0800

    modified prod settings

--- marketplace/settings/prod.py
+++ marketplace/settings/prod.py
@@ -4,8 +4,20 @@ import dj_database_url
 
 DEBUG = False
 
+DATABASE_URL = os.environ.get("DATABASE_URL")
+
 SECRET_KEY = os.environ.get("SECRET_KEY")
 
 ALLOWED_HOSTS = ["market-place-8hq1.onrender.com"]
 
-DATABASES = {"default": dj_database_url.config()}
+RENDER_EXTERNAL_HOSTNAME = os.environ.get("RENDER_EXTERNAL_HOSTNAME")
+if RENDER_EXTERNAL_HOSTNAME:
+    ALLOWED_HOSTS.append(RENDER_EXTERNAL_HOSTNAME)
+
+DATABASES = {
+    "default": dj_database_url.config(
+        # Feel free to alter this value to suit your needs.
+        default=DATABASE_URL,
+        conn_max_age=600,
+    )
+}
