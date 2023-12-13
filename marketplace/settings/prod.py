12aa326df1746a77f4b9def724ee2fea72582c3c
karl ducta <karlducta022@gmail.com>
2023-12-13 16:06:52 +0800

    Implemented AWS S3

--- marketplace/settings/prod.py
+++ marketplace/settings/prod.py
@@ -21,3 +21,15 @@ DATABASES = {
         conn_max_age=600,
     )
 }
+
+
+# AWS S3 settings
+AWS_ACCESS_KEY_ID = os.environ.get("AWS_ACCESS_KEY_ID")
+AWS_SECRET_ACCESS_KEY = os.environ.get("AWS_SECRET_ACCESS_KEY")
+AWS_STORAGE_BUCKET_NAME = os.environ.get("AWS_STORAGE_BUCKET_NAME")
+AWS_S3_REGION_NAME = os.environ.get("AWS_S3_REGION_NAME")
+
+# Use Amazon S3 for storage for uploaded media files.
+DEFAULT_FILE_STORAGE = "storages.backends.s3boto3.S3Boto3Storage"
+
+STATICFILES_STORAGE = "storages.backends.s3boto3.S3Boto3Storage"
