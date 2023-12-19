e8f1e3114dcc2d81745f026e939bef04c37b1242
karl ducta <karlducta022@gmail.com>
2023-12-19 14:28:38 +0800

    added pagination in index and category items

--- core/views.py
+++ core/views.py
@@ -1,12 +1,32 @@
 from django.db.models.aggregates import Count
 from django.shortcuts import redirect, render, get_object_or_404
-
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 from item.models import Category, Item
 from .forms import SignupForm
 
 
 def index(request):
-    items = Item.objects.filter(is_sold="False")[0:6]
+    items_list = Item.objects.filter(is_sold=False)
+
+    # Set the number of items to display per page
+    items_per_page = 6
+
+    # Create a Paginator object
+    paginator = Paginator(items_list, items_per_page)
+
+    # Get the current page number from the request's GET parameters
+    page = request.GET.get("page")
+
+    try:
+        # Get the Page object for the requested page
+        items = paginator.page(page)
+    except PageNotAnInteger:
+        # If page is not an integer, deliver the first page.
+        items = paginator.page(1)
+    except EmptyPage:
+        # If page is out of range (e.g., 9999), deliver the last page of results.
+        items = paginator.page(paginator.num_pages)
+
     categories = Category.objects.annotate(items_count=Count("items"))
     return render(
         request, "core/index.html", context={"categories": categories, "items": items}
@@ -38,7 +58,27 @@ def login(request):
 def category_items(request, category_id):
     category = get_object_or_404(Category, id=category_id)
 
-    items = Item.objects.filter(is_sold="False", category=category)[0:6]
+    items_list = Item.objects.filter(is_sold="False", category=category)[0:6]
+
+    # Set the number of items to display per page
+    items_per_page = 6
+
+    # Create a Paginator object
+    paginator = Paginator(items_list, items_per_page)
+
+    # Get the current page number from the request's GET parameters
+    page = request.GET.get("page")
+
+    try:
+        # Get the Page object for the requested page
+        items = paginator.page(page)
+    except PageNotAnInteger:
+        # If page is not an integer, deliver the first page.
+        items = paginator.page(1)
+    except EmptyPage:
+        # If page is out of range (e.g., 9999), deliver the last page of results.
+        items = paginator.page(paginator.num_pages)
+
     categories = Category.objects.annotate(items_count=Count("items"))
 
     return render(
